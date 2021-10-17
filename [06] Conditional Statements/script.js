657def2f20e5c29f493dbd0b62ff0445906600e5
thao-github <vuthao0410@gmail.com>
2021-10-17 12:03:24 +0700

    up 2 file bt loops

--- [06] Conditional Statements/script.js
+++ [06] Conditional Statements/script.js
@@ -1,19 +1,20 @@
 
-    let inputL=document.getElementById("inputL");
+    let inputL = document.getElementById("inputL");
+
     function pressBtn(obj){
     let pressed = obj.innerHTML;
-    if(pressed=="=") {
-    inputL.innnerHTML  = eval(inputL.innerHTML);
-}
-    else if(pressed == "C") {
-    inputL.innnerHTML = "0";
-}
+    if(pressed === '=') {
+        inputL.innerHTML  = eval(inputL.innerHTML);
+    }
+    else if(pressed === 'C') {
+        inputL.innerHTML = '0';
+    }
     else {
-    if (inputL.innerHTML == 0) {
-    inputL.innerHTML = pressed;
-}
-    else {
-    inputL.innnerHTML += pressed;
-}
-}
+        if (inputL.innerHTML === '0') {
+            inputL.innerHTML = pressed;
+    }
+        else {
+            inputL.innerHTML += pressed;
+        }
+    }
 }
