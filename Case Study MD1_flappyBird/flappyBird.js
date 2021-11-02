484ab6832fd56c0b12bb9fb6be0f0596ed00a5d0
thao-github <vuthao0410@gmail.com>
2021-11-02 10:46:04 +0700

    De2_Module1

--- Case Study MD1_flappyBird/flappyBird.js
+++ Case Study MD1_flappyBird/flappyBird.js
@@ -1,10 +1,7 @@
 const cvs = document.getElementById("canvas");
 const ctx = cvs.getContext("2d");
 
-
-//biến
-// let gap = 75;
-// var constant = pipeNorth.height + gap;
+//variables
 let bX = 10;
 let bY = 150;
 let gravity = 1.5;
@@ -25,7 +22,7 @@ let score1 = new Audio();
 fly.src = "audio/fly.mp3"
 score1.src = "audio/score.mp3"
 
-//tạo pipe
+// pipe
 let pipe = [];
 pipe[0] = {
     x: cvs.width,
@@ -33,7 +30,6 @@ pipe[0] = {
 }
 
 //load images
-
 let bird = new Image();
 const bg = new Image();
 const fg = new Image();
@@ -52,13 +48,13 @@ function draw() {
 
 
     for (let i = 0; i < pipe.length; i++) {
-        const gap = 85;
+        const gap = 150;
         let constant = pipeNorth.height + gap;
         ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
         ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + constant);
         pipe[i].x--;
 
-        if (pipe[i].x === 125) {
+        if (pipe[i].x === 0) {
             pipe.push({
                 x: cvs.width,
                 y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
@@ -66,7 +62,6 @@ function draw() {
         }
 
         //detect collision
-
         if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width
             && (bY <= pipe[i].y + pipeNorth.height || bY + bird.height >= pipe[i].y + constant) ||
             bY + bird.height >= cvs.height - fg.height) {
@@ -75,8 +70,8 @@ function draw() {
 
         //score
         if (pipe[i].x === 5) {
-            score1.play()
             score++;
+            score1.play()
         }
     }
 
@@ -90,7 +85,6 @@ function draw() {
     ctx.fillText("Score: " + score, 10, cvs.height - 20)
 
     requestAnimationFrame(draw);
-
 }
 
 draw();
